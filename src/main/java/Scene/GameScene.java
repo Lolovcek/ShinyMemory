248ca10e752f9e4cdd423960009fd089890a8552
Lolovcek <jakub.bogusciak@gmail.com>
2020-09-12 01:18:04 +0200

    added Scene changing

---
+++ src/main/java/Scene/GameScene.java
@@ -0,0 +1,13 @@
+package Scene;
+
+public class GameScene extends Scene {
+
+    public GameScene() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
---
+++ src/main/java/Scene/MenuScene.java
@@ -0,0 +1,14 @@
+package Scene;
+
+
+public class MenuScene extends Scene {
+
+    public MenuScene() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        System.out.println("FPS is : " + 1/dt);
+    }
+}
