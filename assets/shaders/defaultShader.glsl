3092bc5ec44976366d0ab4b9f7bc5d130c317444
Lolovcek <jakub.bogusciak@gmail.com>
2020-09-16 12:57:32 +0200

    added sprite batch rendering and texture batch rendering

--- assets/shaders/defaultShader.glsl
+++ assets/shaders/defaultShader.glsl
@@ -3,32 +3,37 @@
     layout (location=0) in vec3 attributePos;
     layout (location=1) in vec4 attributeColor;
     layout (location=2) in vec2 attributeTextureCoordinates;
+    layout (location=3) in float attributeTextureId;
 
     uniform mat4 uProjectionMatrix;
     uniform mat4 uViewMatrix;
 
     out vec4 fragmentColor;
     out vec2 fragmentTextureCoordinates;
+    out float fragmentTextureId;
 
     void main() {
         fragmentColor = attributeColor;
         fragmentTextureCoordinates = attributeTextureCoordinates;
+        fragmentTextureId = attributeTextureId;
         gl_Position = uProjectionMatrix * uViewMatrix * vec4(attributePos, 1.0);
     }
     #type fragment
     #version 330 core
 
-    uniform float uTime;
-    uniform sampler2D TEXTURE_SAMPLER;
-
     in vec4 fragmentColor;
     in vec2 fragmentTextureCoordinates;
+    in float fragmentTextureId;
+
+    uniform sampler2D uTextures[8];
 
     out vec4 color;
 
     void main() {
-
-        //float noise = fract(sin(dot(fragmentColor.xy, vec2(12.9898, 78.233))) * 43758.5453);
-        //float avg = (fragmentColor.r + fragmentColor.g + fragmentColor.b) / 3;
-        color = texture(TEXTURE_SAMPLER, fragmentTextureCoordinates); // * noise
+        if (fragmentTextureId > 0) {
+            int id = int(fragmentTextureId);
+            color = fragmentColor * texture(uTextures[id], fragmentTextureCoordinates);
+        } else {
+            color = fragmentColor;
+        }
     }
--- src/main/java/scenes/GameScene.java
+++ src/main/java/scenes/GameScene.java
@@ -1,111 +1,49 @@
 package scenes;
 import camera.Camera;
+import components.SpriteRenderer;
+import entities.GameObject;
+import entities.Transform;
 import org.joml.Vector2f;
-import renderers.Shader;
-import renderers.Texture;
-import utils.Time;
+import utils.AssetPool;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
-import static org.lwjgl.opengl.GL13.glActiveTexture;
 
 
 public class GameScene extends AbstractScene {
 
-    private float[] vertexArray = {
-            // position               // color                          // UV coordinates
-             100.0f, 0.0f,   0.0f,       1.0f, 0.0f, 0.0f, 1.0f,        1, 1,// Bottom right 0
-             0.0f,   100.0f, 0.0f,       0.0f, 1.0f, 0.0f, 1.0f,        0, 0,// Top left     1
-             100.0f, 100.0f, 0.0f ,      0.0f, 0.0f, 1.0f, 1.0f,        1, 0,// Top right    2
-             0.0f,   0.0f,   0.0f,       0.0f, 0.0f, 0.0f, 1.0f,        0, 1// Bottom left  3
-    };
-
-    // IMPORTANT: Must be in counter-clockwise order
-    private int[] elementArray = {
-            2, 1, 0, // Top right triangle
-            0, 1, 3 // bottom left triangle
-    };
-
-    private int vaoID;
-
-    private Shader defaultShader;
-    private Texture testTexture;
-
-    private boolean touchedLeftSide = false, touchedRightSide = false, touchedUpSide = false, touchedBottomSide = true;
-
+    private final float AMOUNT = 200.0f;
     public GameScene() {
 
     }
 
     @Override
     public void init() {
-        this.camera = new Camera(new Vector2f());
+        setCamera(new Camera(new Vector2f()));
 
-        this.defaultShader = new Shader("assets/shaders/defaultShader.glsl");
-        this.defaultShader.compile();
-        this.testTexture = new Texture("assets/images/player.png");
+        GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/goomba.png")));
+        this.addGameObjectToScene(obj1);
 
-        //this.vaoID = this.defaultShader.prepare(this.vertexArray, this.elementArray, 3, 4, 0, 1);
-        this.vaoID = this.defaultShader.prepare(this.vertexArray, this.elementArray, 3, 4, 2, 0, 1, 2);
+        GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)));
+        obj2.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/player.png")));
+        this.addGameObjectToScene(obj2);
+
+        loadResources();
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     }
 
+    private void loadResources() {
+        AssetPool.getShader("assets/shaders/defaultShader.glsl");
+    }
+
     @Override
     public void update(float dt) {
-        // Upload texture to shader
-        defaultShader.uploadTexture("TEXTURE_SAMPLER", 0);
-        glActiveTexture(GL_TEXTURE0);
-        testTexture.bind();
-
-
-        float temp = dt * 100.0f;
-
-        if (touchedBottomSide) {
-            if ((this.camera.position.x - temp) <= -540) {
-                this.camera.position.x = -540;
-                touchedBottomSide = false;
-                touchedRightSide = true;
-                System.out.println("Touched right side at " + this.camera.position.x + " and temp is " + temp);
-            } else {
-                this.camera.position.x -= temp;
-            }
-        } else if (touchedRightSide) {
-            if ((this.camera.position.y - temp) <= -236) {
-                this.camera.position.y = -236;
-                touchedRightSide = false;
-                touchedUpSide = true;
-                System.out.println("Touched up side at " + this.camera.position.y + " and temp is " + temp);
-            }
-            this.camera.position.y -= temp;
-        } else if (touchedUpSide) {
-            if ((this.camera.position.x + temp) >= 0) {
-                this.camera.position.x = 0;
-                touchedUpSide = false;
-                touchedLeftSide = true;
-                System.out.println("Touched left side at " + this.camera.position.x + " and temp is " + temp);
-            }
-            this.camera.position.x += temp;
-        } else if (touchedLeftSide) {
-            if ((this.camera.position.y + temp) >= 0) {
-                this.camera.position.y = 0;
-                touchedLeftSide = false;
-                touchedBottomSide = true;
-                System.out.println("Touched bottom side at " + this.camera.position.y + " and temp is " + temp);
-            }
-            this.camera.position.y += temp;
+        for (GameObject gameObject : getGameObjects()) {
+            gameObject.update(dt);
         }
 
-        this.defaultShader.use();
-
-        //upload variables to the shaders ... must be after using the shader so it's uploaded in the current used shader
-        this.defaultShader.uploadMat4f("uProjectionMatrix", camera.getProjectionMatrix());
-        this.defaultShader.uploadMat4f("uViewMatrix", camera.getViewMatrix());
-        this.defaultShader.uploadFloat("uTime", Time.getTime());
-        this.defaultShader.draw(this.vaoID, this.elementArray.length, 0, 1, 2);
-
-        this.testTexture.unbind();
-        this.defaultShader.detach();
+        getRenderer().render();
     }
 }
